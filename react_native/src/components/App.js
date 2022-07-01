05bc19ff215a192d8f2ca466e7f5f59d09768c59
Kamil Domagalski <domagalski.kamil@gmail.com>
2022-07-01 14:54:19 +0200

    Add buttons to FlatList

--- react_native/src/components/App.js
+++ react_native/src/components/App.js
@@ -7,14 +7,31 @@
  */
 
 import React from 'react';
-import {FlatList, View, StyleSheet, Text} from 'react-native';
+import {Button} from 'react-native-paper';
+import {FlatList, View, StyleSheet, Text, Linking} from 'react-native';
 import courses from '../data/courses.json';
+
+const handleClick = link => {
+  console.log('cilck');
+  Linking.canOpenURL(link).then(supported => {
+    if (supported) {
+      console.log('działa');
+      Linking.openURL(link);
+    } else {
+      console.log("Don't know how to open that URL: " + link);
+    }
+  });
+};
 
 const App = () => (
   <View>
     <FlatList
       data={courses}
-      renderItem={({item}) => <Text>{item.title}</Text>}
+      renderItem={({item}) => (
+        <Button onPress={() => handleClick(item.link)}>
+          Tap to view course
+        </Button>
+      )}
     />
   </View>
 );
