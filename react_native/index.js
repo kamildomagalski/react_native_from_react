d30c61b8a1f991bae9e928bb6d85befc2d8b98c0
Kamil Domagalski <domagalski.kamil@gmail.com>
2022-07-01 12:52:16 +0200

    add paper library for styles and vector-icons library to project

--- react_native/index.js
+++ react_native/index.js
@@ -3,7 +3,16 @@
  */
 
 import {AppRegistry} from 'react-native';
+import React from 'react';
 import App from './src/components/App';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+AppRegistry.registerComponent(appName, () => Main);
