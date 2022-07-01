0fef8fa8573330fa8d7d3c589ff1ae5fbbf04dd2
Kamil Domagalski <domagalski.kamil@gmail.com>
2022-07-01 17:09:38 +0200

    Add styles, add more components to match web view

--- react_native/src/components/App.js
+++ react_native/src/components/App.js
@@ -7,13 +7,15 @@
  */
 
 import React from 'react';
-import {Button} from 'react-native-paper';
-import {FlatList, View, StyleSheet, Text, Linking} from 'react-native';
+import {Button, Card, Title, Paragraph, Appbar} from 'react-native-paper';
+import {FlatList, View, Linking} from 'react-native';
 import courses from '../data/courses.json';
+import styles from '../styles/AppStyles';
 
 const handleClick = link => {
   console.log('cilck');
   Linking.canOpenURL(link).then(supported => {
+    console.log(supported);
     if (supported) {
       console.log('działa');
       Linking.openURL(link);
@@ -25,12 +27,24 @@ const handleClick = link => {
 
 const App = () => (
   <View>
+    <Appbar style={styles.bar}>
+      <Appbar.Content title="Courses" />
+    </Appbar>
     <FlatList
       data={courses}
       renderItem={({item}) => (
-        <Button onPress={() => handleClick(item.link)}>
-          Tap to view course
-        </Button>
+        <Card>
+          <Card.Cover source={{uri: item.image}} />
+          <Card.Content>
+            <Title>{item.title}</Title>
+            <Paragraph>{item.description}</Paragraph>
+          </Card.Content>
+          <Card.Actions>
+            <Button onPress={() => handleClick(item.link)}>
+              Tap to view course
+            </Button>
+          </Card.Actions>
+        </Card>
       )}
     />
   </View>
---
+++ react_native/src/styles/AppStyles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+const AppStyles = StyleSheet.create({
+  bar: {
+    paddingLeft: 50,
+  },
+});
+
+export default AppStyles;
